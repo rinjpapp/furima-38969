c9abccb5fcda77475a5abf07226e4b075c419607
rinjpapp <iamryoinjp@gmail.com>
2023-03-07 20:07:27 +0900

    ページ遷移パス

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :new
 
   def new
+    @item = Item.new
   end
 
   def create
   end
 
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
