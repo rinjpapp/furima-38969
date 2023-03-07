72e8dc93b95eaf6608491117321210b6602e4205
rinjpapp <iamryoinjp@gmail.com>
2023-03-07 19:44:54 +0900

    validation設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do  
+    validates :title
+    validates :content
+    validates :category_id
+    validates :condition_id
+    validates :shipping_option_id
+    validates :prefecture_id
+    validates :shipping_term_id
+  end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
+
+  has_many :items
   
 
 end
