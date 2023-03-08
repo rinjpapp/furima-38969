9746e3c3be12f6e080c60aa0d90b09d59dc39a72
rinjpapp <iamryoinjp@gmail.com>
2023-03-08 21:30:39 +0900

    編集のユーザー制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.order("created_at DESC")
@@ -27,11 +28,23 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:title, :price, :content, :category_id, :condition_id, :shipping_option_id, :prefecture_id, :shipping_term_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if current_user.id != Item.find(params[:id]).user_id
+      redirect_to action: :index
+    end
   end
 end
