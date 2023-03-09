6ede8dd3424b48bccdd222cea10f0293ad1948d9
rinjpapp <iamryoinjp@gmail.com>
2023-03-09 14:40:03 +0900

    商品削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to action: :index
+    end
+  end
+
   private
 
   def item_params
