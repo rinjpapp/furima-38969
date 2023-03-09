5462904a38568559cdd220aaa06b6f1fe068cd1b
rinjpapp <iamryoinjp@gmail.com>
2023-03-09 14:09:19 +0900

    itemコントローラー整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:edit, :show, :update] 
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
     if current_user.id != Item.find(params[:id]).user_id
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
